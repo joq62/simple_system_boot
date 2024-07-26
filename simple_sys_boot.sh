2a6cf97e9fd4a552b2d9bc2b2fd5230a5941a7b7
joq62 <joq62@c50>
2024-07-26 10:17:34 +0200

    corr

--- simple_sys_boot.sh
+++ simple_sys_boot.sh
@@ -3,7 +3,7 @@ rm -rf ctrl
 rm -rf ctrl_dir
 git clone https://github.com/joq62/ctrl.git
 mkdir ctrl_dir
-tar -zxf release/ctrl.tar.gz -C ctrl_dir
+tar -zxf ctrl/release/ctrl.tar.gz -C ctrl_dir
 sudo rm -r ctrl
 ./ctrl_dir/bin/ctrl foreground
-echo "$!" > /run/sys_boot.pid
+sudo echo "$!" > /run/sys_boot.pid
